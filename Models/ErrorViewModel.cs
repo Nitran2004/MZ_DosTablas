7cd79ca9d8e31e9c724cd97a3b1e72f79b8372fc
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-10-18 07:11:42 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MZ_DosTablas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
